e30ee260c6b9a12aefc4de913d2c4e2a1b1652a7
mosaberr911 <mohamed9.saber6@gmail.com>
2024-12-27 19:50:19 +0200

    ..

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-
-        /*fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff*/
+        System.out.println("hello airport");
 
     }
 }
